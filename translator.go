b9e0a42c3266497e6bdfaa6171b5bf2f3f6aaddc
Behzad Shabani <behzad.shabani@gmail.com>
2018-12-31 16:40:32 +0330

    Add translator

---
+++ translator.go
@@ -0,0 +1,34 @@
+package dolphin
+
+import "fmt"
+
+// Dictionary is a map of string to string that represent a key to its translation
+type Dictionary = map[string]string
+
+// Translator is a struct, containing a dictionary
+type Translator struct {
+	dictionary map[string]string
+}
+
+// Translate convert given key
+func (t Translator) Translate(key string, params ...interface{}) string {
+	return fmt.Sprintf(t.dictionary[key], params...)
+}
+
+// NewTranslator generates and returns Translator object
+func NewTranslator(dictionary ...Dictionary) Translator {
+	return Translator{
+		dictionary: mergeDictionary(dictionary...),
+	}
+}
+
+func mergeDictionary(dictionaries ...Dictionary) Dictionary {
+	var dictionary = make(Dictionary)
+	for _, dict := range dictionaries {
+		for key, value := range dict {
+			dictionary[key] = value
+		}
+	}
+
+	return dictionary
+}
